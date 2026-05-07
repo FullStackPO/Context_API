a959b29774c0fd21db492bc6dfca0faa99e7f12b
Harsh Arya <harsharya8946@gmail.com>
2026-05-07 22:41:10 +0530

    routes and folders

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className='bg-gray-900 text-white text-center p-5 font-semibold text-3xl border-b-amber-50'>
+        <h1>Shopping Mart</h1>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
